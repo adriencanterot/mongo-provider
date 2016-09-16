299e032b74a4b837fa76fee0ebc24892e4245658
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-17 00:37:53 +0200

    added config initializer

--- Sources/MongoProvider.swift
+++ Sources/MongoProvider.swift
@@ -3,6 +3,11 @@ import FluentMongo
 
 public class Provider:Vapor.Provider {
     
+    public enum Error:Swift.Error {
+        case noConfigFile
+        case missingConfig(String)
+    }
+    
     public var provided:Providable
     
     public let driver:MongoDriver
@@ -21,8 +26,31 @@ public class Provider:Vapor.Provider {
         self.provided = Providable(database:self.database)
     }
     
-    public convenience init(config:Config) throws {
+    public convenience required init(config:Config) throws {
+        
+        guard let mongodb = config["mongodb"]?.object else {
+            throw Error.noConfigFile
+        }
+        
+        guard let database = mongodb["database"]?.string else {
+            throw Error.missingConfig("database")
+        }
+        
+        guard let user = mongodb["user"]?.string else {
+            throw Error.missingConfig("user")
+        }
+        
+        guard let password = mongodb["password"]?.string else {
+            throw Error.missingConfig("password")
+        }
+        
+        let host = mongodb["host"]?.string ?? "localhost"
+        let port = mongodb["port"]?.int ?? 27017
+        
+        try self.init(database:database, user:user, password:password, host:host, port:port)
         
     }
     
+    public func afterInit(_: Droplet) { }
+    public func beforeServe(_: Droplet) { }
 }
